c1e93c5e0900034edd713b043b7b55fad56c7a0f
Mathan <mathanganjeya@gmail.com>
2020-08-27 10:25:56 +0530

    rooms with chat configured configured the main state to the rooms and you can get the access the main attached rooms id with use of use params

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './SidebarChat.css';
 import { Avatar } from '@material-ui/core';
 import db from './Firebase/firebase';
-
+import { Link } from 'react-router-dom';
 function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState('');
   useEffect(() => {
@@ -20,13 +20,15 @@ function SidebarChat({ id, name, addNewChat }) {
   };
 
   return !addNewChat ? (
-    <div className="sidebarChat">
-      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-      <div className="sidebarChat__info">
-        <h2>{name}</h2>
-        <p>last message..</p>
+    <Link to={`/rooms/${id}`}>
+      <div className="sidebarChat">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="sidebarChat__info">
+          <h2>{name}</h2>
+          <p>last message..</p>
+        </div>
       </div>
-    </div>
+    </Link>
   ) : (
     <div onClick={createChat} className="sidebarChat">
       <h2>Add new chat</h2>
